56792038c4400879348c7e8274e83f0561852257
nathias <n@th1.as>
2018-10-26 10:53:10 +0200

    first example

---
+++ 1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+from pandas_datareader import data
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2015, 1, 1)
+
+df = web.DataReader("XOM", "yahoo", start, end)
+
+print(df.head())
+
+df['Adj Close'].plot()
+
+plt.show()
