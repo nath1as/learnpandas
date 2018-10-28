447ff6d510551ce1ee247cfc3f7e7b98cddc487d
nathias <n@th1.as>
2018-10-28 16:34:08 +0100

    quandl limit exceeded

---
+++ 7.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import quandl
+
+api_key = open('quandlapikey.text', 'r').read()
+
+# df = quandl.get('FMAC/HPI_AK')
+
+print(df.head())
+
+fiddy_states = pd.read_html(
+    'https://simple.wikipedia.org/wiki/List_of_U.S._states'
+    )
+
+# print(fiddy_states[0][1])
+main_df = pd.DataFrame()
+
+for abbv in fiddy_states[0][1][1:]:
+    query = "FMAC/HPI_" + str(abbv)
+    df = quandl.get(query)
+    df.columns = [str(abbv)]
+
+    if main_df.empty:
+        main_df = df
+    else:
+        main_df = main_df.join(df)
+
+print(main_df.head())
+
