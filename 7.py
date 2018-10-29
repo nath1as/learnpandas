c2cd43e51df0ccf7489c6f152032aaf584c6d935
nathias <n@th1.as>
2018-10-29 20:35:37 +0100

    pickling the results

--- 7.py
+++ 7.py
@@ -1,28 +1,38 @@
-import pandas as pd
 import quandl
+import pandas as pd
+import pickle
 
-api_key = open('quandlapikey.text', 'r').read()
+# Not necessary, I just do this so I do not show my API key.
+# api_key = open('quandlapikey.txt','r').read()
 
-# df = quandl.get('FMAC/HPI_AK')
 
-# print(df.head())
+def state_list():
+    fiddy_states = pd.read_html(
+        'https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    return fiddy_states[0][1][1:]
 
-fiddy_states = pd.read_html(
-    'https://simple.wikipedia.org/wiki/List_of_U.S._states'
-    )
 
-# print(fiddy_states[0][1])
-main_df = pd.DataFrame()
+def grab_initial_state_data():
+    states = state_list()
 
-for abbv in fiddy_states[0][1][1:]:
-    query = "FMAC/HPI_" + str(abbv)
-    df = quandl.get(query)
-    df.columns = [str(abbv)]
+    main_df = pd.DataFrame()
 
-    if main_df.empty:
-        main_df = df
-    else:
-        main_df = main_df.join(df)
+    for abbv in states:
+        query = "FMAC/HPI_"+str(abbv)
+        df = quandl.get(query)
+        df.columns = [str(abbv)]
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df)
 
-print(main_df.head())
+    pickle_out = open('fiddy_states.pickle', 'wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()
 
+
+grab_initial_state_data()
+
+pickle_in = open('fiddy_states.pickle', 'rb')
+HPI_data = pickle.load(pickle_in)
+print(HPI_data.head())
